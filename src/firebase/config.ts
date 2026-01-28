ef3936cadf1f7f10b52da1060bcdd5ac8fe7e19c
Rubel Rubel <rr8583118@gmail.com>
2026-01-28 15:16:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-580756327-d8c25",
+  "appId": "1:553935170616:web:0348fd7c6b84b85acdcacd",
+  "apiKey": "AIzaSyCc-Q-4vb7P19xnFcZk_HviqLemvkzUySk",
+  "authDomain": "studio-580756327-d8c25.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "553935170616"
+};
